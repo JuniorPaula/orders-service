4c50372b3f81a8ca5f58f77a29c8283e34ec94e6
Junior Paula <luke.junnior@icloud.com>
2025-03-23 15:38:50 -0300

    Add user entity and resources

---
+++ src/main/java/com/juniorpaula/webserver/resources/UserResouce.java
@@ -0,0 +1,19 @@
+package com.juniorpaula.webserver.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.juniorpaula.webserver.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResouce {
+  
+  @GetMapping
+  public ResponseEntity<User> findAll() {
+    User u = new User(1L, "Maria", "maria@email.com", "99999999", "12345");
+    return ResponseEntity.ok().body(u);
+  }
+}
