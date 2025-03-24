856007273ac6b1c45ab82cdf909a697690fb9703
Junior Paula <luke.junnior@icloud.com>
2025-03-24 16:01:38 -0300

    Implements database exception

--- src/main/java/com/juniorpaula/webserver/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/juniorpaula/webserver/resources/exceptions/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.juniorpaula.webserver.services.exceptions.DatabaseException;
 import com.juniorpaula.webserver.services.exceptions.ResourceNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -18,6 +19,14 @@ public class ResourceExceptionHandler {
   public ResponseEntity<StandardErro> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
     String error = "Resource not found";
     HttpStatus status = HttpStatus.NOT_FOUND;
+    StandardErro err = new StandardErro(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+    return ResponseEntity.status(status).body(err);
+  }
+
+  @ExceptionHandler(DatabaseException.class)
+  public ResponseEntity<StandardErro> database(DatabaseException e, HttpServletRequest request) {
+    String error = "Database error";
+    HttpStatus status = HttpStatus.BAD_REQUEST;
     StandardErro err = new StandardErro(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
     return ResponseEntity.status(status).body(err);
   }
--- src/main/java/com/juniorpaula/webserver/services/UserService.java
+++ src/main/java/com/juniorpaula/webserver/services/UserService.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.juniorpaula.webserver.entities.User;
 import com.juniorpaula.webserver.repositories.UserRepository;
+import com.juniorpaula.webserver.services.exceptions.DatabaseException;
 import com.juniorpaula.webserver.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,13 +32,19 @@ public class UserService {
   }
 
   public void delete(Long id) {
-    repository.deleteById(id);
+    try {
+      findById(id);
+      repository.deleteById(id);
+    }
+    catch (DataIntegrityViolationException e) {
+      throw new DatabaseException(e.getMessage());
+    }
   }
 
   public User update(Long id, User obj) {
     User entity = repository.getReferenceById(id);
     updateData(entity, obj);
-        return repository.save(entity);
+    return repository.save(entity);
   }
 
   private void updateData(User entity, User obj) {
