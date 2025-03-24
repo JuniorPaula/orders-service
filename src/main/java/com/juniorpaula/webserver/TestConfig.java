eb069d1ca0fc39da7db3b8de5c235a40d51ef38d
Junior Paula <luke.junnior@icloud.com>
2025-03-24 10:04:35 -0300

    Implements order status enum

--- src/main/java/com/juniorpaula/webserver/TestConfig.java
+++ src/main/java/com/juniorpaula/webserver/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.juniorpaula.webserver.entities.Order;
 import com.juniorpaula.webserver.entities.User;
+import com.juniorpaula.webserver.entities.enums.OrderStatus;
 import com.juniorpaula.webserver.repositories.OrderRepository;
 import com.juniorpaula.webserver.repositories.UserRepository;
 
@@ -28,9 +29,9 @@ public class TestConfig implements CommandLineRunner {
     User u1 = new User(null, "Maria Brown", "maria@email.com", "988888888", "123456");
     User u2 = new User(null, "Alex Green", "alex@email.com", "977777777", "123456");
   
-    Order o1 = new Order(null, Instant.parse("2025-02-02T15:42:07Z"), u1);
-    Order o2 = new Order(null, Instant.parse("2025-02-03T15:42:07Z"), u2);
-    Order o3 = new Order(null, Instant.parse("2025-02-04T15:42:07Z"), u1);
+    Order o1 = new Order(null, Instant.parse("2025-02-02T15:42:07Z"), OrderStatus.PAID, u1);
+    Order o2 = new Order(null, Instant.parse("2025-02-03T15:42:07Z"), OrderStatus.WAITING_PAYMENT, u2);
+    Order o3 = new Order(null, Instant.parse("2025-02-04T15:42:07Z"), OrderStatus.WAITING_PAYMENT, u1);
 
     userRepository.saveAll(Arrays.asList(u1, u2));
     orderRepository.saveAll(Arrays.asList(o1, o2, o3));
