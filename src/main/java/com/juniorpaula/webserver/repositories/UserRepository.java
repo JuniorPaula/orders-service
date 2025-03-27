d350701750a14e84ce6a0c898b9a334c0a07a7dd
Junior Paula <luke.junnior@icloud.com>
2025-03-27 16:49:08 -0300

    Add authorization service Add security configuration

--- src/main/java/com/juniorpaula/webserver/repositories/UserRepository.java
+++ src/main/java/com/juniorpaula/webserver/repositories/UserRepository.java
@@ -1,9 +1,10 @@
 package com.juniorpaula.webserver.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import com.juniorpaula.webserver.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
+  UserDetails findByEmail(String email);
 }
