0c10e686b82cf35817d61a802e3fc3bad6d87183
Junior Paula <luke.junnior@icloud.com>
2025-03-28 18:20:32 -0300

    Add method to create a new product

--- src/main/java/com/juniorpaula/webserver/resources/ProductResouce.java
+++ src/main/java/com/juniorpaula/webserver/resources/ProductResouce.java
@@ -1,16 +1,23 @@
 package com.juniorpaula.webserver.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.juniorpaula.webserver.dto.ProductDTO;
 import com.juniorpaula.webserver.entities.Product;
 import com.juniorpaula.webserver.services.ProductService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping(value = "/products")
@@ -33,5 +40,13 @@ public class ProductResouce {
     Product obj = service.findById(id);
 
     return ResponseEntity.ok().body(obj);
+  }
+
+  @PostMapping
+  public ResponseEntity<Product> insert(@Valid @RequestBody ProductDTO objDto) {
+
+    Product obj = service.insert(objDto);
+    URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+    return ResponseEntity.created(location).body(obj);
   }
 }
--- src/main/java/com/juniorpaula/webserver/services/ProductService.java
+++ src/main/java/com/juniorpaula/webserver/services/ProductService.java
@@ -6,6 +6,8 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.juniorpaula.webserver.dto.ProductDTO;
+import com.juniorpaula.webserver.entities.Category;
 import com.juniorpaula.webserver.entities.Product;
 import com.juniorpaula.webserver.repositories.ProductRepository;
 
@@ -15,6 +17,9 @@ public class ProductService {
   @Autowired
   private ProductRepository repository;
 
+  @Autowired
+  private CategoryService categoryService;
+
   public List<Product> findAll() {
     return repository.findAll();
   }
@@ -22,5 +27,16 @@ public class ProductService {
   public Product findById(Long id) {
     Optional<Product> obj = repository.findById(id);
     return obj.get();
+  }
+
+  public Product insert(ProductDTO obj) {
+    Product product = new Product(null, obj.name(), obj.description(), obj.price(), obj.imageUrl());
+
+    for (Long id : obj.categoriesIds()) {
+      Category category = categoryService.findById(id);
+      product.getCategories().add(category);
+    }
+
+    return repository.save(product);
   }
 }
