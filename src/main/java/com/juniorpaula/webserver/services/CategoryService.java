e2439d027062dd998e616e99fea4c659d34343a5
Junior Paula <luke.junnior@icloud.com>
2025-03-28 15:56:51 -0300

    throw a resource not found exception if category does not exists

--- src/main/java/com/juniorpaula/webserver/services/CategoryService.java
+++ src/main/java/com/juniorpaula/webserver/services/CategoryService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.juniorpaula.webserver.entities.Category;
 import com.juniorpaula.webserver.repositories.CategoryRepository;
+import com.juniorpaula.webserver.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class CategoryService {
@@ -21,7 +22,7 @@ public class CategoryService {
 
   public Category findById(Long id) {
     Optional<Category> obj = repository.findById(id);
-    return obj.get();
+    return obj.orElseThrow(() -> new ResourceNotFoundException(id));
   }
 
   public Category insert(Category obj) {
