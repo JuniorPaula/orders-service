c6e6a525925b13183aaa31a9fd4a8ce9b26edbea
Junior Paula <luke.junnior@icloud.com>
2025-03-28 10:53:57 -0300

    Replace response User to UserResponseDTO on all routes methods

--- src/main/java/com/juniorpaula/webserver/resources/UserResouce.java
+++ src/main/java/com/juniorpaula/webserver/resources/UserResouce.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.juniorpaula.webserver.dto.UserResponseDTO;
 import com.juniorpaula.webserver.entities.User;
 import com.juniorpaula.webserver.services.UserService;
 
@@ -23,19 +24,33 @@ public class UserResouce {
   private UserService service;
 
   @GetMapping
-  public ResponseEntity<List<User>> findAll() {
+  public ResponseEntity<List<UserResponseDTO>> findAll() {
 
     List<User> users = service.findAll();
 
-    return ResponseEntity.ok().body(users);
+    List<UserResponseDTO> response = users.stream().map(x -> new UserResponseDTO(
+      x.getId(),
+      x.getName(),
+      x.getEmail(),
+      x.getPhone(),
+      x.getRole().toString()
+    )).toList();
+
+    return ResponseEntity.ok().body(response);
   }
 
   @GetMapping(value = "/{id}")
-  public ResponseEntity<User> findById(@PathVariable Long id) {
+  public ResponseEntity<UserResponseDTO> findById(@PathVariable Long id) {
 
     User obj = service.findById(id);
 
-    return ResponseEntity.ok().body(obj);
+    return ResponseEntity.ok(new UserResponseDTO(
+      obj.getId(),
+      obj.getName(),
+      obj.getEmail(),
+      obj.getPhone(),
+      obj.getRole().toString()
+    ));
   }
 
   @DeleteMapping(value = "/{id}")
@@ -47,10 +62,16 @@ public class UserResouce {
   }
 
   @PutMapping(value = "/{id}")
-  public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj) {
+  public ResponseEntity<UserResponseDTO> update(@PathVariable Long id, @RequestBody User obj) {
 
     obj = service.update(id, obj);
 
-    return ResponseEntity.ok().body(obj);
+    return ResponseEntity.ok(new UserResponseDTO(
+      obj.getId(),
+      obj.getName(),
+      obj.getEmail(),
+      obj.getPhone(),
+      obj.getRole().toString()
+    ));
   }
 }
